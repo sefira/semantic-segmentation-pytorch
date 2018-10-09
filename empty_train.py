1d9b0b4719de8a7d081efda52a1d75000142838c
buxingyuan <buxingyuan@baidu.com>
2018-10-09 10:46:16 +0800

    1e7 fake length

---
+++ empty_train.py
@@ -0,0 +1,181 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+# System libs
+import os
+import time
+# import math
+import random
+import argparse
+from distutils.version import LooseVersion
+# Numerical libs
+import torch
+import torch.nn as nn
+# Our libs
+from damage_dataset import TrainDataset
+from models import ModelBuilder, SegmentationModule
+from utils import AverageMeter
+from lib.nn import UserScatteredDataParallel, user_scattered_collate, patch_replication_callback
+import lib.utils.data as torchdata
+
+
+
+# train one epoch
+def train(iterator, history, epoch, args):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+
+    # main loop
+    tic = time.time()
+    for i in range(args.epoch_iters):
+        batch_data = next(iterator)
+        data_time.update(time.time() - tic)
+
+        # measure elapsed time
+        batch_time.update(time.time() - tic)
+        tic = time.time()
+
+        # calculate accuracy, and display
+        if i % args.disp_iter == 0 or (epoch == 13 and i >= 4980):
+            print('Epoch: [{}][{}/{}], Time: {:.2f}, Data: {:.2f}, '
+                  .format(epoch, i, args.epoch_iters,
+                          batch_time.average(), data_time.average()))
+
+
+        # adjust learning rate
+        cur_iter = i + (epoch - 1) * args.epoch_iters
+
+
+def main(args):
+    # Dataset and Loader
+    dataset_train = TrainDataset(
+        args.list_train, args, batch_per_gpu=args.batch_size_per_gpu)
+
+    loader_train = torchdata.DataLoader(
+        dataset_train,
+        batch_size=args.num_gpus,  # we have modified data_parallel
+        shuffle=False,  # we do not use this param
+        collate_fn=user_scattered_collate,
+        num_workers=int(args.workers),
+        drop_last=True,
+        pin_memory=True)
+
+    print('1 Epoch = {} iters'.format(args.epoch_iters))
+
+    # create loader iterator
+    iterator_train = iter(loader_train)
+
+    # Main loop
+    history = {'train': {'epoch': [], 'loss': [], 'acc': []}}
+
+    for epoch in range(args.start_epoch, args.num_epoch + 1):
+        train(iterator_train, history, epoch, args)
+
+    print('Training Done!')
+
+
+if __name__ == '__main__':
+    assert LooseVersion(torch.__version__) >= LooseVersion('0.4.0'), \
+        'PyTorch>=0.4.0 is required'
+
+    parser = argparse.ArgumentParser()
+    # Model related arguments
+    parser.add_argument('--id', default='baseline',
+                        help="a name for identifying the model")
+    parser.add_argument('--arch_encoder', default='resnet50_dilated8',
+                        help="architecture of net_encoder")
+    parser.add_argument('--arch_decoder', default='ppm_bilinear_deepsup',
+                        help="architecture of net_decoder")
+    parser.add_argument('--weights_encoder', default='',
+                        help="weights to finetune net_encoder")
+    parser.add_argument('--weights_decoder', default='',
+                        help="weights to finetune net_decoder")
+    parser.add_argument('--fc_dim', default=2048, type=int,
+                        help='number of features between encoder and decoder')
+
+    # Path related arguments
+    parser.add_argument('--list_train',
+                        default='./data/train.odgt')
+    parser.add_argument('--list_val',
+                        default='./data/validation.odgt')
+    parser.add_argument('--root_dataset',
+                        default='./data/')
+
+    # optimization related arguments
+    parser.add_argument('--num_gpus', default=8, type=int,
+                        help='number of gpus to use')
+    parser.add_argument('--batch_size_per_gpu', default=2, type=int,
+                        help='input batch size')
+    parser.add_argument('--num_epoch', default=20, type=int,
+                        help='epochs to train for')
+    parser.add_argument('--start_epoch', default=1, type=int,
+                        help='epoch to start training. useful if continue from a checkpoint')
+    parser.add_argument('--epoch_iters', default=5000, type=int,
+                        help='iterations of each epoch (irrelevant to batch size)')
+    parser.add_argument('--optim', default='SGD', help='optimizer')
+    parser.add_argument('--lr_encoder', default=2e-2, type=float, help='LR')
+    parser.add_argument('--lr_decoder', default=2e-2, type=float, help='LR')
+    parser.add_argument('--lr_pow', default=0.9, type=float,
+                        help='power in poly to drop LR')
+    parser.add_argument('--beta1', default=0.9, type=float,
+                        help='momentum for sgd, beta1 for adam')
+    parser.add_argument('--weight_decay', default=1e-4, type=float,
+                        help='weights regularizer')
+    parser.add_argument('--deep_sup_scale', default=0.4, type=float,
+                        help='the weight of deep supervision loss')
+    parser.add_argument('--fix_bn', default=0, type=int,
+                        help='fix bn params')
+
+    # Data related arguments
+    parser.add_argument('--num_class', default=6, type=int,
+                        help='number of classes')
+    parser.add_argument('--workers', default=16, type=int,
+                        help='number of data loading workers')
+    parser.add_argument('--imgSize', default=[300,375,450,525,600], nargs='+', type=int,
+                        help='input image size of short edge (int or list)')
+    parser.add_argument('--imgMaxSize', default=1000, type=int,
+                        help='maximum input image size of long edge')
+    parser.add_argument('--padding_constant', default=8, type=int,
+                        help='maxmimum downsampling rate of the network')
+    parser.add_argument('--segm_downsampling_rate', default=8, type=int,
+                        help='downsampling rate of the segmentation label')
+    parser.add_argument('--random_flip', default=True, type=bool,
+                        help='if horizontally flip images when training')
+
+    # Misc arguments
+    parser.add_argument('--seed', default=304, type=int, help='manual seed')
+    parser.add_argument('--ckpt', default='./ckpt',
+                        help='folder to output checkpoints')
+    parser.add_argument('--disp_iter', type=int, default=20,
+                        help='frequency to display')
+
+    args = parser.parse_args()
+    print("Input arguments:")
+    for key, val in vars(args).items():
+        print("{:16} {}".format(key, val))
+
+    args.batch_size = args.num_gpus * args.batch_size_per_gpu
+    args.max_iters = args.epoch_iters * args.num_epoch
+    args.running_lr_encoder = args.lr_encoder
+    args.running_lr_decoder = args.lr_decoder
+
+    args.id += '-' + str(args.arch_encoder)
+    args.id += '-' + str(args.arch_decoder)
+    args.id += '-ngpus' + str(args.num_gpus)
+    args.id += '-batchSize' + str(args.batch_size)
+    args.id += '-imgMaxSize' + str(args.imgMaxSize)
+    args.id += '-paddingConst' + str(args.padding_constant)
+    args.id += '-segmDownsampleRate' + str(args.segm_downsampling_rate)
+    args.id += '-LR_encoder' + str(args.lr_encoder)
+    args.id += '-LR_decoder' + str(args.lr_decoder)
+    args.id += '-epoch' + str(args.num_epoch)
+    args.id += '-decay' + str(args.weight_decay)
+    args.id += '-fixBN' + str(args.fix_bn)
+    print('Model ID: {}'.format(args.id))
+
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    main(args)
